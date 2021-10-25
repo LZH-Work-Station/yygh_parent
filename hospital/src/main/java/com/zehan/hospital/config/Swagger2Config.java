201d1426605458bea16e24df1dbbad75504afd24
LZH-WORK-STATION <zehan_li@163.com>
2021-10-26 00:44:08 +0800

    Add yygh-manage

--- hospital/src/main/java/com/zehan/hospital/config/Swagger2Config.java
+++ hospital/src/main/java/com/zehan/hospital/config/Swagger2Config.java
@@ -1,18 +1,25 @@
-package com.zehan.yygh.common.config;
+package com.zehan.hospital.config;
 
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Swagger2配置信息
+ * @author qy
  */
 @Configuration
 @EnableSwagger2
@@ -25,21 +32,10 @@ public class Swagger2Config {
                 .groupName("webApi")
                 .apiInfo(webApiInfo())
                 .select()
-                //只显示api路径下的页面
-                .paths(Predicates.and(PathSelectors.regex("/api/.*")))
-                .build();
-
-    }
-
-    @Bean
-    public Docket adminApiConfig(){
-
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("adminApi")
-                .apiInfo(adminApiInfo())
-                .select()
-                //只显示admin路径下的页面
-                .paths(Predicates.and(PathSelectors.regex("/admin/.*")))
+                //过滤掉admin路径下的所有页面
+                .paths(Predicates.and(PathSelectors.regex("/P2P/.*")))
+                //过滤掉所有error或error.*页面
+                //.paths(Predicates.not(PathSelectors.regex("/error.*")))
                 .build();
 
     }
@@ -50,17 +46,7 @@ public class Swagger2Config {
                 .title("网站-API文档")
                 .description("本文档描述了网站微服务接口定义")
                 .version("1.0")
-                .contact(new Contact("zehan", "http://zehan.com", "493211102@qq.com"))
-                .build();
-    }
-
-    private ApiInfo adminApiInfo(){
-
-        return new ApiInfoBuilder()
-                .title("后台管理系统-API文档")
-                .description("本文档描述了后台管理系统微服务接口定义")
-                .version("1.0")
-                .contact(new Contact("zehan", "http://zehan.com", "49321112@qq.com"))
+                .contact(new Contact("qy", "http://zehan.com", "55317332@qq.com"))
                 .build();
     }
 
---
+++ hospital/src/main/java/com/zehan/hospital/mapper/HospitalSetMapper.java
@@ -0,0 +1,13 @@
+package com.zehan.hospital.mapper;
+
+import com.zehan.hospital.model.HospitalSet;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+
+}
